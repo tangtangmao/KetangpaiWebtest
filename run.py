34683dbe5cd8254d217023214836654114e5e1b2
haoxuejun <1185375204@qq.com>
2020-04-29 15:20:08 +0800

    修改了run文件

--- run.py
+++ run.py
@@ -2,7 +2,15 @@
 #-*-coding:utf-8 -*-
 
 #author:haoxuejun
-# pytest -s -q test_loginpage_pytest.py --alluredir ../OutPuts/allurereport
 
 #生成allure的测试报告
 # allure serve ../OutPuts/allurereport
+import pytest
+import sys
+print("开始测试")
+#执行pytest前的参数准备
+pytest_execute_params = ['-v','--alluredir','OutPuts/allurereport/','--clean-alluredir']
+
+exit_code = pytest.main(pytest_execute_params)
+print('结束测试')
+sys.exit(exit_code)
